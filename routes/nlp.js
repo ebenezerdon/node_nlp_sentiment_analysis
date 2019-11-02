5a960b6dea29a1a745f703a1e35e03370a823ed1
Ebenezer Don <ebenezerdonu@gmail.com>
2019-11-02 16:56:02 +0100

    add spell corrector

--- routes/nlp.js
+++ routes/nlp.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const aposToLexForm = require('apos-to-lex-form');
-const { SentimentAnalyzer, PorterStemmer, WordTokenizer } = require('natural');
+const natural = require('natural');
+const SpellCorrector = require('spelling-corrector');
 const SW = require('stopword');
 
 const router = express.Router();
 
+const spellCorrector = new SpellCorrector();
+spellCorrector.loadDictionary();
+
 router.post('/s-analyzer', function(req, res, next) {
+  const { review } = req.body;
   /* NORMALIZATION */
 
   // negation handling
   // convert apostrophe=connecting words to lex form
-  const lexedReview = aposToLexForm(req.body.review);
+  const lexedReview = aposToLexForm(review);
 
   // casing
   const casedReview = lexedReview.toLowerCase();
@@ -19,19 +24,26 @@ router.post('/s-analyzer', function(req, res, next) {
   const alphaOnlyReview = casedReview.replace(/[^a-zA-Z\s]+/g, '');
 
   // tokenize review
+  const { WordTokenizer } = natural;
   const tokenizer = new WordTokenizer();
   const tokenizedReview = tokenizer.tokenize(alphaOnlyReview);
+
+  // spell correction
+  tokenizedReview.forEach((word, index) => {
+    tokenizedReview[index] = spellCorrector.correct(word);
+  })
+
+  console.log('------------------------------------');
+  console.log(tokenizedReview);
+  console.log('------------------------------------');
 
   // remove stopwords
   const filteredReview = SW.removeStopwords(tokenizedReview);
 
+  const { SentimentAnalyzer, PorterStemmer } = natural;
   const analyzer = new SentimentAnalyzer('English', PorterStemmer, 'afinn');
 
   const analysis = analyzer.getSentiment(filteredReview);
-
-  console.log('------------------------------------');
-  console.log(analysis);
-  console.log('------------------------------------');
 
   res.status(200).json({ analysis });
 });
